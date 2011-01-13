0b22268c93a028fe5caa54f43c62bb1b717edf73
Abhishek Yadav <abhishek@tenmiles.com>
2011-01-13 19:00:59 +0530

    WIP - converting the plugin to gem

--- blog/vendor/plugins/preview/init.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), "lib", "preview")
-
-# Ask ActiveRecord::Base to include code in the Preview::Models module
-# Execute the ActiveRecord::Base.include method with Preview::Models arg
-#ActiveRecord::Base.send(:include, Preview::Models)
-
-# Ask ActionController::Base to include Preivew::Controller::InstanceMethods
-ActionController::Base.send(:include, Preview::Controllers)
